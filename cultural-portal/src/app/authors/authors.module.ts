f67e614051c68603e2c432d2c4672d6959271bef
VlaDrozd <vladrozd2000@gmail.com>
2020-03-04 21:09:22 +0300

    feat: add basic project structure and add tslint

---
+++ cultural-portal/src/app/authors/authors.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthorsRoutingModule } from './authors-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AuthorsRoutingModule
+  ]
+})
+export class AuthorsModule { }
---
+++ cultural-portal/src/app/team/team.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TeamRoutingModule } from './team-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TeamRoutingModule
+  ]
+})
+export class TeamModule { }
