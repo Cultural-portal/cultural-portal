52d9e448fee6f0bf28ed6a3bdeba68c8042a574c
VlaDrozd <vladrozd2000@gmail.com>
2020-03-04 21:18:40 +0300

    feat: implement author interface

--- cultural-portal/src/app/authors/models/author.model.ts
+++ cultural-portal/src/app/authors/models/author.model.ts
@@ -1,2 +1,17 @@
 export interface Author {
+  name: string;
+  birth: Date;
+  death?: Date;
+  img: string;
+  biography: Biography[];
+  works: string[];
+  video?: string;
+  place?: string;
+}
+
+export interface Biography {
+  startDate: Date;
+  endDate: Date;
+  title: string;
+  text: string;
 }
