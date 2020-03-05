27ea57edfec8404d803613c55e88399ef6a2ea55
Aliaksandr Babtsou <babtsoualiaksandr@gmail.com>
2020-03-05 16:40:29 +0300

    feat: add routing to list athors

--- cultural-portal/src/app/app-routing.module.ts
+++ cultural-portal/src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'authors', loadChildren: () => import('./authors/authors.module').then(m => m.AuthorsModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
