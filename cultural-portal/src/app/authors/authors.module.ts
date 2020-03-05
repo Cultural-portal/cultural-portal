06a1ac08a89934d24454771e612b34ff9dcbc13d
Aliaksandr Babtsou <babtsoualiaksandr@gmail.com>
2020-03-05 19:47:41 +0300

    feat: add component author-item

--- cultural-portal/src/app/authors/authors.module.ts
+++ cultural-portal/src/app/authors/authors.module.ts
@@ -5,13 +5,17 @@ import { AuthorsRoutingModule } from './authors-routing.module';
 import { AuthorsListComponent } from './pages/authors-list/authors-list.component';
 import { AuthorInfoComponent } from './pages/author-info/author-info.component';
 import { SearchPipe } from './pipes/search.pipe';
+import { SharedModule } from '../shared/shared.module';
+import { AutorItemComponent } from './components/autor-item/autor-item.component';
+
 
 
 @NgModule({
-  declarations: [AuthorsListComponent, AuthorInfoComponent, SearchPipe],
+  declarations: [AuthorsListComponent, AuthorInfoComponent, SearchPipe, AutorItemComponent],
   imports: [
     CommonModule,
-    AuthorsRoutingModule
+    AuthorsRoutingModule,
+    SharedModule
   ]
 })
 export class AuthorsModule { }
--- cultural-portal/src/app/shared/shared.module.ts
+++ cultural-portal/src/app/shared/shared.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatListModule} from '@angular/material/list';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatListModule
+  ],
+  exports: [
+    MatListModule
   ]
 })
 export class SharedModule { }
