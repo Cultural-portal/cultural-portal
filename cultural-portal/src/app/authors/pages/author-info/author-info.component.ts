8c94d177deb5710c00d8fa5a2f591316437c4cbb
mrinex <mrinex@mail.ru>
2020-03-05 23:09:12 +0300

    feat: add some details author

--- cultural-portal/src/app/authors/pages/author-info/author-info.component.ts
+++ cultural-portal/src/app/authors/pages/author-info/author-info.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Authors, AuthorsRoot } from '../../models/author.model';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-author-info',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthorInfoComponent implements OnInit {
 
-  constructor() { }
+  public authorsEN: Authors[];
+  public authorsRU: Authors[];
+  public authorsBE: Authors[];
+  public author: Authors;
+  public map: string;
+  // @Input() author: Authors; // for list
+  constructor( private http: HttpClient ) { }
 
   ngOnInit(): void {
+    let authors = this.getJsonAuthors();
+    authors.subscribe((data: AuthorsRoot) => {
+      this.authorsEN = data.authorsEN;
+      this.authorsRU = data.authorsRU;
+      this.authorsBE = data.authorsBE;
+    });
   }
+
+  public getJsonAuthors() {
+    return this.http.get('/assets/data/authors.json');
+  }
+
+  public getVideo(n) {
+    let response = this.http.get(n);
+    response.subscribe( video => console.log(video));
+  }
+
+  public getMap(cord) {
+    const URL_API = 'https://api.opencagedata.com/';
+    const KEY = '12ff4fe1ac804a4689043079fcfc5b48';
+
+    const url = `${URL_API}geocode/v1/map?q=${cord}&key=${KEY}&pretty=1&no_annotations=1&abbrv=1`;
+    const response = this.http.get(url);
+    response.subscribe((map: {url: string}) => this.map = map.url);
+    return this.map;
+  }
+  // import { AuthorInfoComponent } from './authors/pages/author-info/author-info.component';
+  // import { HttpClientModule } from '@angular/common/http';
 
 }
