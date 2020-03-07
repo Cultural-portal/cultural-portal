122dcaf38d31c598e4eeaff86d35f4f3fabfb17b
Aliaksandr Babtsou <babtsoualiaksandr@gmail.com>
2020-03-07 20:58:45 +0300

    feat: route to author/info?author

--- cultural-portal/src/app/authors/authors-routing.module.ts
+++ cultural-portal/src/app/authors/authors-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthorsListComponent } from './pages/authors-list/authors-list.component';
+import { AuthorInfoComponent } from './pages/author-info/author-info.component';
 
 
 const routes: Routes = [
-  {path: 'list', component: AuthorsListComponent}
+  {path: 'list', component: AuthorsListComponent},
+  {path: 'info', component: AuthorInfoComponent }
 ];
 
 @NgModule({
