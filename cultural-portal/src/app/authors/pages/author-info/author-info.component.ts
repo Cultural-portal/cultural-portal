835838be007e56c0a1caaf5d389cfb8a18673ab8
mrinex <mrinex@mail.ru>
2020-03-08 22:53:00 +0300

    refactor: full screen

--- cultural-portal/src/app/authors/pages/author-info/author-info.component.ts
+++ cultural-portal/src/app/authors/pages/author-info/author-info.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Authors, AuthorsRoot } from '../../models/author.model';
-import { HttpClient } from '@angular/common/http';
-// import { SafeUrl } from "@angular/platform-browser";
 import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
@@ -10,38 +7,34 @@ import { DomSanitizer } from "@angular/platform-browser";
   styleUrls: ['./author-info.component.scss']
 })
 export class AuthorInfoComponent implements OnInit {
-  // urlVideo: SafeUrl;
-  url = "https://www.youtube.com/embed/";
-  public author: Authors;
-  public res;
+  
   // @Input() author: Authors; // for list
-  constructor( private sanitizer: DomSanitizer, private http: HttpClient ) { }
+  constructor( private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
-    let authors = this.getJsonAuthors();
-    authors.subscribe((data: AuthorsRoot) => {
-      this.author = data.authorsRU[0];
-    });
-  }
-
-  public getJsonAuthors() {
-    return this.http.get('/assets/data/authors.json');
   }
 
   public getVideo(video) {
     return this.sanitizer.bypassSecurityTrustResourceUrl(
-      `${this.url}${video}`
+      `https://www.youtube.com/embed/${video}`
     );
+  }
+
+  public full() {
+    let elem = document.querySelector('.video');
+    if (!document.fullscreenElement) {
+      elem.requestFullscreen().catch(err => {
+        console.log(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
+      });
+    } else {
+      document.exitFullscreen();
+    }
   }
 
   public getMap(cord) {
     const URL_API = 'https://api.opencagedata.com/';
     const KEY = '12ff4fe1ac804a4689043079fcfc5b48';
-    const show = `https://api.opencagedata.com/geocode/v1/map?q=${cord}&key=12ff4fe1ac804a4689043079fcfc5b48&pretty=1&no_annotations=1&abbrv=1`
-    // const url = `${URL_API}geocode/v1/map?q=${cord}&key=${KEY}&pretty=1&no_annotations=1&abbrv=1`;
-    // let response = this.http.get(url);
-    // response.subscribe((i: any) => console.log('-------------2:',i.url));
-    console.log('show:',show);
+    const show = `${URL_API}geocode/v1/map?q=${cord}&key=${KEY}&pretty=1&no_annotations=1&abbrv=1`
     return this.sanitizer.bypassSecurityTrustResourceUrl(
       show
     );
