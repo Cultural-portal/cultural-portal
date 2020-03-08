5e53021f2e14584904aadcb5f5f843ffcfb8a564
VlaDrozd <vladrozd2000@gmail.com>
2020-03-08 15:21:23 +0300

    refactor: add information about all developers

--- cultural-portal/src/app/team/pages/developers/developers.component.ts
+++ cultural-portal/src/app/team/pages/developers/developers.component.ts
@@ -8,36 +8,44 @@ import { Developer } from '../../models/developer.model';
 })
 export class DevelopersComponent implements OnInit {
 
-  public developers: Developer[] = [{
-    name: 'Vladislav Drozd',
-    github: 'https://github.com/VlaDrozd',
-    img: 'https://avatars2.githubusercontent.com/u/25082511?s=460&v=4',
-    role: 'developer'
-  },
-  {
-    name: 'Vladislav Drozd',
-    github: 'https://github.com/VlaDrozd',
-    img: 'https://avatars2.githubusercontent.com/u/25082511?s=460&v=4',
-    role: 'developer'
-  },
-  {
-    name: 'Vladislav Drozd',
-    github: 'https://github.com/VlaDrozd',
-    img: 'https://avatars2.githubusercontent.com/u/25082511?s=460&v=4',
-    role: 'developer'
-  },
-  {
-    name: 'Vladislav Drozd',
-    github: 'https://github.com/VlaDrozd',
-    img: 'https://avatars2.githubusercontent.com/u/25082511?s=460&v=4',
-    role: 'developer'
-  },
-  {
-    name: 'Vladislav Drozd',
-    github: 'https://github.com/VlaDrozd',
-    img: 'https://avatars2.githubusercontent.com/u/25082511?s=460&v=4',
-    role: 'developer'
-  }];
+  public developers: Developer[] = [
+    {
+      name: 'Vladislav Drozd',
+      github: 'https://github.com/VlaDrozd',
+      img: 'https://avatars2.githubusercontent.com/u/25082511?s=460&v=4',
+      role: 'developer'
+    },
+    {
+      name: 'Timur Yermakou',
+      github: 'https://github.com/errmakovv',
+      img: 'https://avatars1.githubusercontent.com/u/47751940?s=460&v=4',
+      role: 'developer'
+    },
+    {
+      name: 'Ulyana Bychkovskaya',
+      github: 'https://github.com/ulyana18',
+      img: 'https://avatars3.githubusercontent.com/u/44649203?s=460&v=4',
+      role: 'developer'
+    },
+    {
+      name: 'Aliaksandr	Babtsou',
+      github: 'https://github.com/babtsoualiaksandr',
+      img: 'https://avatars3.githubusercontent.com/u/25074606?s=460&v=4',
+      role: 'developer'
+    },
+    {
+      name: 'Mikita	Maiseichyk',
+      github: 'https://github.com/Vromga',
+      img: 'https://avatars2.githubusercontent.com/u/48679402?s=460&v=4',
+      role: 'developer'
+    },
+    {
+      name: 'Uladzimir	Kazak',
+      github: 'https://github.com/mrinex',
+      img: 'https://avatars0.githubusercontent.com/u/35580404?s=460&v=4',
+      role: 'developer'
+    }
+  ];
 
   constructor() { }
 
