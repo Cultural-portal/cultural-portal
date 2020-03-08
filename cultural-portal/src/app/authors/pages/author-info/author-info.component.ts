9fe4755563acd133a45b05f004ff814cf0284ee4
mrinex <mrinex@mail.ru>
2020-03-08 20:42:36 +0300

    feat: add video and map show

--- cultural-portal/src/app/authors/pages/author-info/author-info.component.ts
+++ cultural-portal/src/app/authors/pages/author-info/author-info.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Authors, AuthorsRoot } from '../../models/author.model';
 import { HttpClient } from '@angular/common/http';
+// import { SafeUrl } from "@angular/platform-browser";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-author-info',
@@ -8,21 +10,17 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./author-info.component.scss']
 })
 export class AuthorInfoComponent implements OnInit {
-
-  public authorsEN: Authors[];
-  public authorsRU: Authors[];
-  public authorsBE: Authors[];
+  // urlVideo: SafeUrl;
+  url = "https://www.youtube.com/embed/";
   public author: Authors;
-  public map: string;
+  public res;
   // @Input() author: Authors; // for list
-  constructor( private http: HttpClient ) { }
+  constructor( private sanitizer: DomSanitizer, private http: HttpClient ) { }
 
   ngOnInit(): void {
     let authors = this.getJsonAuthors();
     authors.subscribe((data: AuthorsRoot) => {
-      this.authorsEN = data.authorsEN;
-      this.authorsRU = data.authorsRU;
-      this.authorsBE = data.authorsBE;
+      this.author = data.authorsRU[0];
     });
   }
 
@@ -30,21 +28,23 @@ export class AuthorInfoComponent implements OnInit {
     return this.http.get('/assets/data/authors.json');
   }
 
-  public getVideo(n) {
-    let response = this.http.get(n);
-    response.subscribe( video => console.log(video));
+  public getVideo(video) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(
+      `${this.url}${video}`
+    );
   }
 
   public getMap(cord) {
     const URL_API = 'https://api.opencagedata.com/';
     const KEY = '12ff4fe1ac804a4689043079fcfc5b48';
-
-    const url = `${URL_API}geocode/v1/map?q=${cord}&key=${KEY}&pretty=1&no_annotations=1&abbrv=1`;
-    const response = this.http.get(url);
-    response.subscribe((map: {url: string}) => this.map = map.url);
-    return this.map;
+    const show = `https://api.opencagedata.com/geocode/v1/map?q=${cord}&key=12ff4fe1ac804a4689043079fcfc5b48&pretty=1&no_annotations=1&abbrv=1`
+    // const url = `${URL_API}geocode/v1/map?q=${cord}&key=${KEY}&pretty=1&no_annotations=1&abbrv=1`;
+    // let response = this.http.get(url);
+    // response.subscribe((i: any) => console.log('-------------2:',i.url));
+    console.log('show:',show);
+    return this.sanitizer.bypassSecurityTrustResourceUrl(
+      show
+    );
   }
-  // import { AuthorInfoComponent } from './authors/pages/author-info/author-info.component';
-  // import { HttpClientModule } from '@angular/common/http';
 
 }
