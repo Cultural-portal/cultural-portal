6b8a7529419a14e5e44bbf5249cdaa1e87ed5bb5
VlaDrozd <vladrozd2000@gmail.com>
2020-03-09 21:19:45 +0300

    feat: add redirect

--- cultural-portal/src/app/app-routing.module.ts
+++ cultural-portal/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'main', pathMatch: 'prefix' },
   { path: 'authors', loadChildren: () => import('./authors/authors.module').then(m => m.AuthorsModule) },
   { path: 'team', loadChildren: () => import('./team/team.module').then(m => m.TeamModule) },
   { path: 'main', loadChildren: () => import('./core/core.module').then(m => m.CoreModule) }
