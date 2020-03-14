44dd08ed615197a149a5ec9fb439e7b0ed2631a6
Aliaksandr Babtsou <babtsoualiaksandr@gmail.com>
2020-03-14 12:03:06 +0300

    feat: translation teams and style guards

--- cultural-portal/src/app/shared/pipes/translate.pipe.ts
+++ cultural-portal/src/app/shared/pipes/translate.pipe.ts
@@ -1,54 +1,8 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Language } from 'src/app/core/models/language.enum';
 import { StateService } from '../services/state.service';
-import { Observable } from 'rxjs';
+import { Dictionary } from '../data/dictionary';
 
-
-export const Dictionary = {
-  ru: {
-    'Choose lang': 'Выбрать язык',
-    'ru': 'руc',
-    'en': 'англ',
-    'be': 'бел',
-    'Search': 'Поиск',
-    'name': 'Имя',
-    'city': 'город',
-    'all': 'без фильтра',
-    'enter text': 'введи текст' ,
-    'More information': 'Больше информации',
-    'List of Authors': 'Список авторов',
-    'Team members': 'Наша команда',
-    'Worklog': 'Журнал работы',
-    'Styleguide': 'Инструкция по стилю',
-    'About' : 'О программе',
-    'Created by ': 'Сделано',
-    'GROUP-3 ': 'Крутая группа №3',
-    "Angular Course 2020 ": 'Ангуляр курсы 2020',
-    'Poets Of Belarus': 'Поэты Беларуси'
-
-  },
-  be: {
-    'Choose lang': 'выбраць мову',
-    'ru': 'руc',
-    'en': 'англ',
-    'be': 'бел',
-    'all': 'без фільтра',
-    'Search': 'Пошук',
-    'name': 'Iмя',
-    'city': 'горад',
-    'enter text': 'увядзi тэкст',
-    'More information': 'Больш інфармацыі',
-    'List of Authors': 'Спiс автарау',
-    'Team members': 'Удзельнікі каманды',
-    'Worklog': 'Часопіс працы',
-    'Styleguide': 'Інструкцыя па стылі',
-    'About' : 'Пра праграму',
-    'Created by ': "Створана ",
-    'GROUP-3 ': ' Група-3 ',
-    "Angular Course 2020": 'Вуглавы курс 2020',
-    'Poets Of Belarus' : 'Паэты Беларусі'
-  },
- };
 
 @Pipe({
   name: 'translate'
